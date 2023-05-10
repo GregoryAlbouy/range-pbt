97bb9ead8cef344074a4eb481ac6473a13cd5dd7
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2023-05-10 12:03:11 +0200

    Additional test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -15,12 +15,21 @@ describe("Test of range()", () => {
     expect(range(5)).toEqual([1, 2, 3, 4, 5]);
   });
 
-  test("n-length range", () => {
+  test("sum", () => {
     fc.assert(
       fc.property(fc.nat({ max: 100 }), (n) => {
         const suite = range(n);
         const reversedSuite = [...suite].reverse();
         return suite.reduce(sum, 0) + reversedSuite.reduce(sum, 0) === n * (n + 1);
+      })
+    );
+  });
+
+  test("exact values", () => {
+    fc.assert(
+      fc.property(fc.nat({ max: 100 }), (n) => {
+        const suite = range(n);
+        return range(n).every((i) => suite[i - 1] === i);
       })
     );
   });
